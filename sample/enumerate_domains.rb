cbf4cbe40765b1fc6930566a8678a8d88d0edbd5
Eric Hodel <drbrain@segment7.net>
2009-08-12 23:11:44 -0700

    Exit when we've got them all

--- sample/enumerate_domains.rb
+++ sample/enumerate_domains.rb
@@ -2,8 +2,14 @@ require 'dnssd'
 
 Thread.abort_on_exception = true
 
+domains = []
+
 enumerator = DNSSD.enumerate_domains do |reply|
-  p reply.domain
+  domains << reply.domain
+  next if reply.flags.more_coming?
+
+  puts "Found domains:\n#{domains.join "\n"}"
+  exit
 end
 
 trap 'INT' do enumerator.stop; exit end
