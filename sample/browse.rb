51091ea76902896ebd0073f857b010b57b07cb69
Eric Hodel <drbrain@segment7.net>
2009-08-12 23:17:40 -0700

    Use more_coming?

--- sample/browse.rb
+++ sample/browse.rb
@@ -1,7 +1,17 @@
 require 'dnssd'
 
+services = []
+
 browser = DNSSD.browse '_presence._tcp' do |reply|
-  p reply
+  services << reply
+  next if reply.flags.more_coming?
+
+  puts "Presence services found:"
+  services.each do |service|
+    puts "#{service.name} on #{service.domain}"
+  end
+
+  exit
 end
 
 trap 'INT' do browser.stop; exit end
