27361e045a68559c7e03d76f98683d8bf54788ba
Eric Hodel <drbrain@segment7.net>
2009-08-17 21:39:07 -0700

    Remove duplicate display of text_record

--- sample/resolve_ichat.rb
+++ sample/resolve_ichat.rb
@@ -1,14 +1,14 @@
 require 'dnssd'
-require 'pp'
 
 Thread.abort_on_exception = true
+trap 'INT' do exit end
+trap 'TERM' do exit end
 
 class ChatNameResolver
   def self.resolve_add(reply)
     Thread.new reply do |reply|
       DNSSD.resolve reply.name, reply.type, reply.domain do |resolve_reply|
         puts "Adding: #{resolve_reply.inspect}"
-        pp resolve_reply.text_record
         resolve_reply.service.stop
       end
     end
@@ -23,16 +23,13 @@ class ChatNameResolver
   end
 end
 
-browser = DNSSD.browse '_presence._tcp' do |reply|
+DNSSD.browse '_presence._tcp' do |reply|
   if reply.flags.add? then
     ChatNameResolver.resolve_add reply
   else
     ChatNameResolver.resolve_remove reply
   end
 end
-
-trap 'INT' do browser.stop; exit end
-trap 'TERM' do browser.stop; exit end
 
 sleep
 
