32a6caef03c33fd8c2e0b3c0bcc9e5c1171ca21b
Eric Hodel <drbrain@segment7.net>
2009-08-17 21:42:46 -0700

    Add helpful hint

--- sample/socket.rb
+++ sample/socket.rb
@@ -1,9 +1,10 @@
 require 'dnssd'
 
 Thread.abort_on_exception = true
-
 trap 'INT'  do exit end
 trap 'TERM' do exit end
+
+puts 'run sample/server.rb'
 
 service = nil
 
@@ -17,4 +18,5 @@ puts "found service #{service.name}"
 socket = service.connect
 
 puts "Connected to %s:%d" % socket.peeraddr.values_at(2, 1)
+puts "        from %s:%d" % socket.addr.values_at(2, 1)
 
