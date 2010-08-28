d07fd8a31ae2294c4b56b38804dd89f7cb192295
Eric Hodel <drbrain@segment7.net>
2010-08-29 07:32:14 +0900

    Move test into the correct file

--- test/test_dnssd_service.rb
+++ test/test_dnssd_service.rb
@@ -8,5 +8,20 @@ class TestDNSSDService < MiniTest::Unit::TestCase
                    DNSSD::Service.get_property(DNSSD::Service::DaemonVersion)
   end if DNSSD::Service.respond_to? :get_property
 
+  def test_class_getaddrinfo
+    service = DNSSD::Service.new
+
+    addresses = []
+    begin
+      service.getaddrinfo 'localhost' do |addrinfo|
+        addresses << addrinfo.address
+        break unless addrinfo.flags.more_coming?
+      end
+    ensure
+      service.stop
+    end
+    assert addresses.index('127.0.0.1')
+  end
+
 end
 
