d6a2086df07a24bade66d312ce7faa9be2f51c6c
Eric Hodel <drbrain@segment7.net>
2010-09-07 01:29:08 -0700

    Remove whitespace

--- test/test_dnssd_reply_query_record.rb
+++ test/test_dnssd_reply_query_record.rb
@@ -13,37 +13,37 @@ class TestDNSSDReplyQueryRecord < MiniTest::Unit::TestCase
 
   def test_record_data_A
     qr = util_qr DNSSD::Record::A, @ipv4
-    
+
     assert_equal IPAddr.new_ntoh(@ipv4), qr.record_data
   end
 
   def test_record_data_AAAA
     qr = util_qr DNSSD::Record::A, @ipv6
-    
+
     assert_equal IPAddr.new_ntoh(@ipv6), qr.record_data
   end
 
   def test_record_data_CNAME
     qr = util_qr DNSSD::Record::CNAME, @nowhere
-    
+
     assert_equal 'nowhere.example.', qr.record_data
   end
 
   def test_record_data_MX
     qr = util_qr DNSSD::Record::MX, "\000\010#{@nowhere}"
-    
+
     assert_equal [8, 'nowhere.example.'], qr.record_data
   end
 
   def test_record_data_NS
     qr = util_qr DNSSD::Record::NS, @nowhere
-    
+
     assert_equal 'nowhere.example.', qr.record_data
   end
 
   def test_record_data_PTR
     qr = util_qr DNSSD::Record::PTR, @nowhere
-    
+
     assert_equal 'nowhere.example.', qr.record_data
   end
 
@@ -62,7 +62,7 @@ class TestDNSSDReplyQueryRecord < MiniTest::Unit::TestCase
       'nowhere.example.', 'me.nowhere.example.',
       serial, refresh, rtry, expire, minimum
     ]
-    
+
     assert_equal expected, qr.record_data
   end
 
@@ -74,13 +74,13 @@ class TestDNSSDReplyQueryRecord < MiniTest::Unit::TestCase
     data = "#{[priority, weight, port].pack 'nnn'}#{@nowhere}"
 
     qr = util_qr DNSSD::Record::SRV, data
-    
+
     assert_equal [1, 5, 1025, 'nowhere.example.'], qr.record_data
   end
 
   def test_record_data_TXT
     qr = util_qr DNSSD::Record::TXT, "\005Hello\006World!"
-    
+
     assert_equal %w[Hello World!], qr.record_data
   end
 
