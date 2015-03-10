5a9e476f6dad36a5228632be606353b89331fdee
Aaron Patterson <aaron.patterson@gmail.com>
2015-03-09 18:48:38 -0700

    should probably add this file

---
+++ test/helper.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'dnssd'
+require 'socket'
+require 'thread'
+require 'monitor'
+require 'securerandom'
+
+class Latch
+  def initialize(count = 1)
+    @count = count
+    @lock = Monitor.new
+    @cv = @lock.new_cond
+  end
+
+  def release
+    @lock.synchronize do
+      @count -= 1 if @count > 0
+      @cv.broadcast if @count.zero?
+    end
+  end
+
+  def await
+    @lock.synchronize do
+      @cv.wait_while { @count > 0 }
+    end
+  end
+end
+
+module DNSSD
+  class Test < Minitest::Test
+  end
+end
