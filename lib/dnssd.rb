dd42d60c6f4889b0e1ad5f25cdce61c7ae0eeb72
Aaron Patterson <aaron.patterson@gmail.com>
2015-04-03 11:28:15 +0530

    bump version

--- lib/dnssd.rb
+++ lib/dnssd.rb
@@ -13,7 +13,7 @@ module DNSSD
   ##
   # The version of DNSSD you're using.
 
-  VERSION = '3.0.0'
+  VERSION = '3.0.1'
 
   ##
   # Registers +socket+ with DNSSD as +name+.  If +service+ is omitted it is
